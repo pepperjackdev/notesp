a1599c73169e0edb52b7f6fcf8a345844f866ca6
Giacomo De Florio <giacomodeflorio9093@gmail.com>
2025-01-31 20:45:01 +0100

    initial commit

---
+++ core/src/test/java/me/pepperjackdev/notesp/AppTest.java
@@ -0,0 +1,14 @@
+package me.pepperjackdev.notesp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void rigorousTest() {
+        assertTrue(true);
+    }
+
+}
